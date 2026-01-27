52df45e2cd68100e563bf8a7a47476399eaa93f9
hrolfr <sabrina.sales163@gmail.com>
2026-01-27 19:36:22 -0300

    mais usuarios adicionados

--- INSERT E SELECT.sql
+++ INSERT E SELECT.sql
@@ -1,5 +1,12 @@
+--INSERT INTO usuarios (id, nome, email, data_nascimento, endereco)
+--VALUES
+--(1, 'Pâmela Apolinario', 'teste.123@gmail.com', '1991-10-07', 'Avenida dos Tupinambas, 153 - Vila Rocha/SP' );
+
+--SELECT * FROM usuarios
+
 INSERT INTO usuarios (id, nome, email, data_nascimento, endereco)
 VALUES
-(1, 'Pâmela Apolinario', 'teste.123@gmail.com', '1991-10-07', 'Avenida dos Tupinambas, 153 - Vila Rocha/SP' );
+(2, 'Ângela Souza', 'teste.234@gmail.com', '1991-10-25', 'Avenida Maria das Dores, 11200 - Vila Lupercio/MG' ),
+(3, 'José Weidemann Pires', 'teste.345@gmail.com', '1969-05-01', 'Rua Denilson Bonifácil, 22 - Vila Rosa/MT'),
+(4, 'Odete Soares Santos de Cunha', 'teste.456@gmail.com', '1958-11-15', 'Rua Engenheiro Ademir Costa, 1300 - Vila Joelma/MT');
 
-SELECT * FROM usuarios
