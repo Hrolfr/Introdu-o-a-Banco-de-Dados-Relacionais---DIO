199e1353f718a99405c560b60a91890daef77720
hrolfr <sabrina.sales163@gmail.com>
2026-01-27 18:34:26 -0300

    Arquivo de Inserts e Selects adicionado juntamente com o primeiro usuário adicionado

---
+++ INSERT E SELECT.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (id, nome, email, data_nascimento, endereco)
+VALUES
+(1, 'Pâmela Apolinario', 'teste.123@gmail.com', '1991-10-07', 'Avenida dos Tupinambas, 153 - Vila Rocha/SP' );
+
+SELECT * FROM usuarios
