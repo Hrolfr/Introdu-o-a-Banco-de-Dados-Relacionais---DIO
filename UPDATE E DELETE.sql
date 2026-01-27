3548c43545f490c6ecf104d07d319dfc565a7949
hrolfr <sabrina.sales163@gmail.com>
2026-01-27 20:05:02 -0300

    delete + consulta realizados e documento finalizado

--- UPDATE E DELETE.sql
+++ UPDATE E DELETE.sql
@@ -1,7 +1,14 @@
+--1
 UPDATE usuarios
 set id = 4
 WHERE email = 'teste.123@gmail.com'
 
 SELECT * FROM usuarios
-WHERE
-email like 'teste.123@gmail.com';
+WHERE email like 'teste.123@gmail.com';
+
+--2
+DELETE FROM usuarios
+WHERE nome = 'Praia do Rosa';
+
+SELECT * FROM usuarios
+WHERE nome = 'Praia do Rosa';
